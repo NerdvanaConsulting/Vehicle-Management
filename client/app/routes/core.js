f9999459cc6deba6eee12af0d20ad9db4e90a0ca
NerdvanaConsulting <bardpolley@nerdvanaconsulting.com>
2018-12-25 10:14:32 -0500

    Issue 12 - Dashboard routing (#13)

---
+++ client/app/routes/core.js
@@ -0,0 +1,14 @@
+/************************************************
+ * Application:  Vehicle Manager
+ * File:  app/routes/core.js
+ * Author: Nerdvana Consulting
+ * Creation Date: 24 Dec 2018
+ * Requirement 12 - core route (for dashboard)
+ * Purpose: This is the container route for the application
+ * Modification History:
+ * - 24 Dec 2018 - Initial build (no modifications)
+ ***********************************************/
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+});
