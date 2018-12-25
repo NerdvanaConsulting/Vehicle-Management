4578e6c14c60af427f0a870efe32059d2bcb079b
NerdvanaConsulting <bardpolley@nerdvanaconsulting.com>
2018-12-25 10:28:22 -0500

    Merge Development into master to set up for initial acceptance testing framework (#15)
    * Updating initial documentation for contribution and pull requests
    
    * Updating initial documentation for contribution and pull requests
    
    * Continuous Integration Installation
    
    * Continuous Integration Installation
    
    * Skeleton CI files to populate when ready to integrate
    
    * Update issue templates
    
    * Template Updates (#3)
    
    * Pre-development Documentation (#1)
    
    * Updating initial documentation for contribution and pull requests
    
    * Updating initial documentation for contribution and pull requests
    
    * PR push initial documentation to master (#2)
    
    * Updating initial documentation for contribution and pull requests
    
    * Updating initial documentation for contribution and pull requests
    
    * Continuous Integration Installation
    
    * Continuous Integration Installation
    
    * Skeleton CI files to populate when ready to integrate
    
    * Issue 12 - Dashboard routing (#13)

---
+++ client/app/routes/core/dashboard.js
@@ -0,0 +1,14 @@
+/************************************************
+ * Application:  Vehicle Manager
+ * File:  app/routes/core/dashboard.js
+ * Author: Nerdvana Consulting
+ * Creation Date: 24 Dec 2018
+ * Requirement 12 - Dashboard route
+ * Purpose: This is the route for the dashboard
+ * Modification History:
+ * - 24 Dec 2018 - Initial build (no modifications)
+ ***********************************************/
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+});
