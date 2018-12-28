4b76ad03a664fdfdea28aae0223ff02cb4585be5
Bardley Polley <polleyb@iupui.edu>
2018-12-28 16:42:28 -0500

    Added sample data within application adapter and route for vehicle manager

--- client/app/models/vehicle.js
+++ client/app/models/vehicle.js
@@ -7,17 +7,17 @@
  * Purpose: This is the model for a vehicle
  * Modification History:
  * - 26 Dec 2018 - Initial build (no modifications)
+ * - 28 Dec 2018 - Removed id attribute 
  ***********************************************/
 import DS from 'ember-data';
 
 export default DS.Model.extend({
    type: DS.attr('string'),
-   id: DS.attr('string'),
    attributes: {
       vin: DS.attr('string'),
       vehMake: DS.attr('string'),
       vehModel: DS.attr('string'),
       vehYear: DS.attr('number'),
       vehColor: DS.attr('string'),
-   }s
+   }
 });
